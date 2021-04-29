e8ea15aa10ca51d8c39cfe65073a2951ff910658
fauzan falah <fauzan1892@codekop.com>
2021-04-29 15:10:13 +0700

    learn 29-Apr-2021

---
+++ Mobile First Approach/off_canvas_menu/script.js
@@ -0,0 +1,14 @@
+const hamburgerButtonElement = document.querySelector("#hamburger");
+const drawerElement = document.querySelector("#drawer");
+const mainElement = document.querySelector("main");
+ 
+hamburgerButtonElement.addEventListener("click", event => {
+    drawerElement.classList.toggle("open");
+    event.stopPropagation();
+});
+ 
+ 
+mainElement.addEventListener("click", event => {
+    drawerElement.classList.remove("open");
+    event.stopPropagation();
+})
