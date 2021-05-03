331c15d883ef91b078313ded126341aefe148825
Fauzan Falah <fauzan1892@codekop.com>
2021-05-03 13:51:47 +0700

    cek

---
+++ restaurant-apps/src/scripts/index.js
@@ -0,0 +1,7 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../styles/responsive.css';
+
+import downMenu from './downMenu.js';
+
+downMenu();
